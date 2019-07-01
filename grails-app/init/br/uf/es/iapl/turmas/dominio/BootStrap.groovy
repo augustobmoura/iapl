ca08ac2f5070b5d9fd85c5bdebb924ab680aa0ee
jpcabral <jpocabral@gmail.com>
2019-07-01 20:22:43 -0300

    correções mistas

--- grails-app/init/br/uf/es/iapl/turmas/dominio/BootStrap.groovy
+++ grails-app/init/br/uf/es/iapl/turmas/dominio/BootStrap.groovy
@@ -3,13 +3,14 @@ package br.uf.es.iapl.turmas.dominio
 class BootStrap {
 
     def init = { servletContext ->
+        cadastraAdminSeNecessario()
     }
     def destroy = {
     }
 
     def cadastraAdminSeNecessario() {
-        if (Usuario.findByEmail('augusto.borgesm@gmail.com')) {
-            new Professor(email: 'augusto.borgesm@gmail.com', senha: 'admin', )
+        if (Usuario.findByEmail('jpocabral@gmail.com')) {
+            new Professor(email: 'jpocabral@gmail.com', senha: 'admin', )
         }
     }
 }
